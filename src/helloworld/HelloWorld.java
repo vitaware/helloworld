9c4263d95ebc5ba5aab6f16adcc99cf23fb50a71
tom <reretom@126.com>
2017-12-28 21:16:13 +0800

    test modified code

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello git world");
+		System.out.println("hello git world ÐÞ¸ÄÒ»ÏÂ");
 
 	}
 
